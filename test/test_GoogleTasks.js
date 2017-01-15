46a72969cc696e72e8c3941353fb5c936ee38e48
BEN5756 <bennewsome5756.bn@gmail.com>
2017-01-15 18:48:08 +0000

    Created basic bad test suit

---
+++ test/test_GoogleTasks.js
@@ -0,0 +1,60 @@
+// tests/test_GoogleTasks.js
+
+
+var chai = require('chai');
+var expect = chai.expect;
+var GoogleTasks = require('./../GoogleTasks.js');
+
+
+describe('getAuth', function() {
+//   var foo=false;
+
+//   beforeEach(function(done){
+//      setTimeout(function() {
+//        gauth = googleTasks.getGoogleAuth(SECRET_FOLDER, done)
+         // complete the async beforeEach
+//         done();
+//      }, 5000);
+//   });
+
+
+   it('Testing creation of gauth object', function(done) {
+      var googleTasks = new GoogleTasks;
+      var SECRET_FOLDER = "./"
+      googleTasks.getGoogleAuth(SECRET_FOLDER, gotGoogleAuth);
+
+      function gotGoogleAuth(gauth) {
+         console.log("Returned authorisation:");
+         console.log(gauth);
+         done();
+         }
+//      done();
+
+//      console.log(GoogleTasks.gauth);
+//      expect(gauth).to.equal(1);
+   });
+});
+
+describe('getTasks', function() {
+
+   it('Testing getting tasks list', function(done) {
+
+      var googleTasks = new GoogleTasks;
+      var SECRET_FOLDER = "./";
+      googleTasks.getGoogleAuth(SECRET_FOLDER, gotGoogleAuth);
+
+      function printTasks(list) {
+         console.log("Recived the following task list");
+         console.log(list);
+         done();
+      }
+
+      function gotGoogleAuth(gauth) {
+         console.log("Returned authorisation:");
+         console.log(gauth);
+         googleTasks.updateTasks(gauth, printTasks)
+         }
+   });
+});
+
+   
